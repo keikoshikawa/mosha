069b9ad6ac2497b6f1ac4c5a85aab91b56702688
kei.koshikawa <kei.koshikawa1@gmail.com>
2021-12-05 18:17:49 +0900

    背景完成

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(function() {
     $('.btn-gNav').on("click", function() {        // js-btnクラスをクリックすると、
       $(this).toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
-      $('#gNav , .btn-line').toggleClass('open');
+      $('#gNav , .btn-line , .has-sub').toggleClass('open');
     });
   });
   
-  
+$(function(){
+  $(".has-sub").hover(function(){      // .has-subボタンをホバーすると
+    $(this).children(".sub").slideToggle(200); // .subメニューがスライドで出る(アコーデオン)
+  });
+});
